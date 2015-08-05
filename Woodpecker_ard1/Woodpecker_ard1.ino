90cbdcbb3ed2836e22aff83f9a5ba082e583694b
drehleierguy <ben@macrophone.org>
2015-08-05 15:04:24 -0400

    Make trigger area a little larger
    By 4 “cm”, whatever those are.

--- Woodpecker_ard1/Woodpecker_ard1.ino
+++ Woodpecker_ard1/Woodpecker_ard1.ino
@@ -2,7 +2,7 @@
 
 DistanceGP2Y0A21YK Dist;
 
-int distance = 10;        // THIS IS WHERE YOU ADJUST THE DISTANCE THRESHOLD - the higher the number, the farther away it will look for a trigger
+int distance = 14;        // THIS IS WHERE YOU ADJUST THE DISTANCE THRESHOLD - the higher the number, the farther away it will look for a trigger
 
 const int dirpin = 12;    // Tells the driver what direction to rotate.
 const int steppin = 13;   // Tells the driver to step.
